f207e95ca7103a7aff45631c3fdeff16b3446599
Dewmini Jayasinghe <153798474+dewminijayasinghe@users.noreply.github.com>
2024-06-07 15:35:12 +0530

    Initial Commit

---
+++ gif.py
@@ -0,0 +1,9 @@
+import imageio.v3 as iio
+
+filenames = ['team-pic1.png','team-pic2.png']
+images = [ ]
+
+for filename in filenames:
+  images.append(iio.imread(filename))
+
+iio.imwrite('team.gif', images, duration = 500, loop = 0)
